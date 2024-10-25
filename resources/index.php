e8227a4a1219cf94f64deb47da89ae6df1089158
pads-oculars <pads_oculars0y@icloud.com>
2024-10-25 15:53:40 +0300

    Updated resources

--- resources/index.php
+++ resources/index.php
@@ -3,3 +3,4 @@ Act as if what you do makes a difference. It does. Success doesn’t just find you
 CI/CD Success usually comes to those who are too busy to be looking for it. Agile Distributed Systems Success doesn’t just find you. You have to go out and get it. RabbitMQ If you can dream it, you can do it. Sharding A/B Testing Kubernetes
 DNS Routing Latency Optimization Event Sourcing Lambda Functions Cloud Microservices Success doesn’t just find you. You have to go out and get it.
 Database Kafka Multi-threading Data Pipeline Cloud Functions Pull Request Machine Learning
+API Gateway Work hard in silence, let your success be your noise. Parallel Processing Event Sourcing Optimization Message Queue Success doesn’t just find you. You have to go out and get it. Load Balancing Backend Kubernetes
